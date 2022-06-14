639564ed8b0ad61d6e20e882a86219ab4dfe510b
DevAdedeji <adedejitewogbade2@gmail.com>
2022-06-14 13:24:17 +0100

    Some configs done

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 function Register() {
   document.title = "Register an account on Tweeter"
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const registerAccount = (e) =>{
+    e.preventDefault()
+    console.log(email, password);
+  }
   return (
-    <div>Register</div>
+    <div>
+      <form onSubmit={registerAccount}>
+        <div>
+          <label htmlFor="email">Email Address:</label>
+          <input type="email" value={email} onChange={(e)=> setEmail(e.target.value)} required/>
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" value={password} onChange={(e)=> setPassword(e.target.value)} required/>
+        </div>
+        <button>Register</button>
+      </form>
+    </div>
   )
 }
 
